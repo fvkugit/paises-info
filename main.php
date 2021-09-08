390e0ddc06213b049ebc171228d6d310e9090884
Facundo <facugastonbarral@gmail.com>
2021-09-08 16:38:14 -0300

    update main.php

--- main.php
+++ main.php
@@ -15,6 +15,7 @@ $respuesta = curl_exec($curl);
 $err = curl_error($curl);
 curl_close($curl);
 $respuesta = json_decode($respuesta, true);
+if (array_key_exists('status', $respuesta) && $respuesta['status'] == 404){header("Location: error.html");die();}
 $respuesta = $respuesta[0];
 $info = new stdClass(); 
 $info->nombre = ("Pais: {$respuesta['name']}");
@@ -26,5 +27,22 @@ $info->reg = ("Region: {$respuesta['region']}");
 $info->subreg = ("Sub-region: {$respuesta['subregion']}");
 $info->pob = ("Población: {$respuesta['population']}");
 $info->area = ("Area: {$respuesta['area']}");
-print_r($info)
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title><?php echo($info->nombre)?></title>
+</head>
+<body>
+    <table>
+        <?php
+        echo("<tr><th><h1>{$info->nombre}</h1></th></tr>");
+        foreach($info as $clave => $valor) {if ($clave == 'nombre'){continue;};echo("<tr><td>{$valor}</td></tr>");}
+        ?>
+    </table>
+    <form><input style="margin: auto; display: block; padding: 5px 30px;" type="button" value="Volver" onclick="history.back()"></form>
+</body>
+</html>
